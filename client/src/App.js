f3847c7853fba2f012427b14e37137fddd9bf444
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-09-07 00:03:39 +0400

    Wired up react router and added some Link tags for navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
-function App() {
+const PageOne = () => {
+      return <div>
+          Here's the first page 
+          <Link to="/pagetwo">Go to page 2</Link>
+          </div>
+}
+
+const PageTwo = () => {
+    return <div>
+            Here's the second page
+            <Link to='/'>Back to page 1</Link>
+          </div>
+}
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route path='/' exact component={PageOne} />
+            <Route path='/pagetwo' component={PageTwo} />
+          </div>
+        </BrowserRouter>
+      </div>
+  )
 }
 
 export default App;
