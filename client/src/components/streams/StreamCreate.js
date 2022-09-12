3202d2a33f0b82c2e08ca179ecbfaad6c6cfe230
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-09-12 22:24:34 +0400

    handled validation and text submission

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -12,16 +12,39 @@ class StreamCreate extends React.Component {
         )
     }
 
+    //create a helper function for submission
+    onSubmit(formValues) {
+        console.log(formValues)
+    }
+
+
     render() {
         return (
-            <form className="ui form">
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
                 <Field name="title" component={this.renderInput} label="Add a title" />
                 <Field name="description" component={this.renderInput} label="Add a description" />
-                <button className="ui button" type="submit">Create</button>
+                <button className="ui button primary" type="submit">Create</button>
             </form>
         )
     }
 }
+
+//create a validation function
+const validate = (formValues) => {
+    //define an errors object 
+    const errors ={}
+
+    if (!formValues.title) {
+        return errors.title = 'Please enter a title'
+    } 
+
+    if (!formValues.description) {
+        return errors.description = 'Please enter a description'
+    }
+
+    return errors
+}
+
 
 export default reduxForm({
     form: 'stream_create'
