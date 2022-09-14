69ff25f898fbd9570c977c3324c545010fa406c8
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-09-15 01:53:45 +0400

    Created all the needed action creators for all the API requests

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -13,7 +13,69 @@ export const signOut = () => {
     }
 }
 
+//add action creators for REST API operations
+
+export const createNewStream = () => {
+    return {
+        type: 'CREATE_STREAM'
+    }
+}
+
+export const listAllStreams = () => {
+    return {
+        type: 'LIST_ALL_STREAMS'
+    }
+}
+
+export const getOneStream = () => {
+    return {
+        type: 'GET_ONE_STREAM'
+    }
+
+}
+
+export const updateStream = () => {
+    return {
+        type: 'UPDATE_STREAM'
+    }
+}
+
+export const deleteStream = () => {
+    return { 
+        type: 'DELETE_STREAM' 
+    }
+}
+
+
+
 //create new action creator to handle stream creation and POST requests
 export const createStream = formValues => async dispatch => {
-    streams.post('/streams', formValues)
+    const response = await streams.post('/streams', formValues)
+
+    dispatch({ createNewStream, payload: response.data })
+}
+
+export const fetchAllStreams = () => async dispatch => {
+    const response = await streams.get('/streams')
+
+    dispatch({ fetchAllStreams, payload: response.data })
+}
+
+export const fetchOneStream = (id) => async dispatch =>{
+    const response = await streams.get(`/streams/${id}`)
+
+    dispatch({ fetchOneStream, payload: response.data })
+
+}
+
+export  const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues)
+
+    dispatch({ editStream, payload: response.data })
+}
+
+export  const removeStream = (id) => async dispatch => {
+    await streams.delete(`/streams/${id}`)
+
+    dispatch({ removeStream, payload: id})
 }
