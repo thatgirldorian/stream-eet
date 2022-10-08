768c42dfae11bb73fdb14dc052986f45cdd7fd8e
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-10-08 21:57:32 +0400

    Added a way to list all streams using the mapkeys function from lodash

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,9 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
 import _ from 'lodash';
 
 //Create an object-based reducer for streaming using the actions provided
 
 export default (state = {}, action) => {
     switch (action.type) {
+        case 'LIST_ALL_STREAMS':
+            return {...state, ..._.mapKeys(action.payload, 'id') }
+
         case 'GET_ONE_STREAM': 
             return {...state, [action.payload.id]: action.payload }
 
@@ -15,9 +19,6 @@ export default (state = {}, action) => {
 
         case 'DELETE_STREAM':
             return _.omit(state, action.payload)
-
-        case 'LIST_ALL_STREAMS':
-            return {...state, [action.payload.id]: action.pay}
 
         default:
             return state
