ff408859ead4ac51d707202a89640e6e7c2925b6
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-10-08 21:50:44 +0400

    Added a way for the stream reducer to create, read, update and delete (using lodash)

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,3 +1,25 @@
-//Create an object-based reducer 
+import _ from 'lodash';
 
-import index from '../actions'
+//Create an object-based reducer for streaming using the actions provided
+
+export default (state = {}, action) => {
+    switch (action.type) {
+        case 'GET_ONE_STREAM': 
+            return {...state, [action.payload.id]: action.payload }
+
+        case 'CREATE_STREAM':
+            return {...state, [action.payload.id]: action.payload }
+
+        case 'UPDATE_STREAM':
+            return {...state, [action.payload.id]: action.payload }
+
+        case 'DELETE_STREAM':
+            return _.omit(state, action.payload)
+
+        case 'LIST_ALL_STREAMS':
+            return {...state, [action.payload.id]: action.pay}
+
+        default:
+            return state
+    }
+}
