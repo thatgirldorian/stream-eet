f5580980a1b25555ef7e8c282ef75f6b6f8e5095
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-10-10 00:13:31 +0400

    Added a way to edit streams + fixed header navigation

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import '../App.css'
 import GoogleAuth from './GoogleAuth'
 
 const Header = () => {
     return (
-        <BrowserRouter>
             <div className="ui secondary pointing menu">
-                <Link to='/' className="item">stream-eet</Link>
+                <Link to="/" className="item">stream-eet</Link>
                 <div className="right menu">
-                    <Link to='/streams/list' className="item">All Streams</Link>
+                    <Link to="/" className="item">All Streams</Link>
                     <GoogleAuth />
                 </div>
             </div>
-        </BrowserRouter>
     )
 }
 
--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchOneStream } from '../../actions'
 
-const StreamEdit = () => {
-    return <div>Edit a stream</div>
+class StreamEdit extends React.Component {
+    componentDidMount() {
+        this.props.fetchOneStream(this.props.match.params.id)
+    }
+
+    render() {
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        }
+        return(
+            <div>
+                <h2 className="stream-header">Edit stream</h2>
+                <div>{this.props.stream.title}</div>
+            </div>
+        )
+    } 
+
 }
 
-export default StreamEdit
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, {fetchOneStream})(StreamEdit)
