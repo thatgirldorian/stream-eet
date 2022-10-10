b52aa86fc19bf277f6a6abb7a74deda7df646012
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-10-11 00:42:31 +0400

    Fixed the delete button link to pull in the stream's id

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Modal from '../modal';
 import history from '../../history'
+import { fetchOneStream, editStream } from '../../actions'
 
 const StreamDelete = () => {
     //pull in action buttons 
@@ -27,5 +28,37 @@ const StreamDelete = () => {
         </div>
     ) 
 }
+
+
+// class StreamDelete extends React.Component {
+//     //pull in action buttons 
+//     // const actions = (
+//     //     <React.Fragment>
+//     //         <button className="ui red button">Delete</button>
+//     //         <button 
+//     //             onClick={() => history.push('/')}
+//     //             className="ui cancel button">Cancel</button>
+//     //     </React.Fragment>
+//     // )
+
+    
+//     render() {
+//         return (
+//             <div>
+//             <h2 className="stream-header">Delete Stream</h2>
+//             <Modal
+//                 title="Delete Stream"
+//                 content="Are you sure you want to delete this stream? Please note this action is permanent and you will not be able to restore your stream."
+//                 // actions={actions}
+//                 onDismiss = {() => history.push('/')}
+            
+//             />
+//         </div>
+//         )
+        
+    
+//     }
+// }
+
 
 export default StreamDelete
