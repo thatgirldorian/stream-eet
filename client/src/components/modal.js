eaa37c891d2796dab4ea74481590d825e986246f
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-10-11 00:29:46 +0400

    made the modal component reusable

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history'
 
 const Modal = props => {
     return ReactDOM.createPortal(
         <div 
-        onClick={() => history.push('/')}
+        onClick={props.onDismiss}
         className="ui dimmer modals visible active">
             <div 
             onClick={(e) => e.stopPropagation()}
             className="ui standard modal visible active">
-            <div className="header">Delete a stream</div>
-            <div className="content">Are you sure you want to delete this stream? Please note this action is permanent and you will not be able to restore your stream.</div>
-            <div className="actions">
-                <div className="ui red button">Delete</div>
-                <div 
-                onClick={() => history.push('/')}
-                className="ui cancel button">Cancel</div>
-            </div>
+            <div className="header">{props.title}</div>
+            <div className="content">{props.content}</div>
+            <div className="actions">{props.actions}</div>
             </div>
         </div>,
         //add a reference to the element to render this modal into
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Modal from '../modal';
+import history from '../../history'
 
 const StreamDelete = () => {
+    //pull in action buttons 
+    const actions = (
+        <React.Fragment>
+            <button className="ui red button">Delete</button>
+            <button 
+                onClick={() => history.push('/')}
+                className="ui cancel button">Cancel</button>
+        </React.Fragment>
+    )
+
+    
     return(
         <div>
             <h2 className="stream-header">Delete Stream</h2>
-            <Modal />
+            <Modal
+                title="Delete Stream"
+                content="Are you sure you want to delete this stream? Please note this action is permanent and you will not be able to restore your stream."
+                actions={actions}
+                onDismiss = {() => history.push('/')}
+            
+            />
         </div>
     ) 
 }
