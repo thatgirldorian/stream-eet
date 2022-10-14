9511eda1b07b741229055ae1c5c6157756135d43
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-10-14 16:41:20 +0400

    added a way to play streams via a flv player

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import flv from 'flv.js'
 import { connect } from 'react-redux'
 import { fetchOneStream } from '../../actions';
 
 class StreamShow extends React.Component {
+//define a constructor to set up a video reference
+constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef()
+}
+
     componentDidMount() {
-        this.props.fetchOneStream(this.props.match.params.id)
+        const { id } = this.props.match.params
+
+        this.props.fetchOneStream(id)
+        this.buildPlayer()
+    }
+
+    //for when the component rerenders/updates
+    componentDidUpdate() {
+        this.buildPlayer()
+    }
+
+    //use this to clean up any resources created by the video player
+    componentWillUnmount() {
+
+    }
+
+    buildPlayer() {
+        if (this.player || !this.props.stream) {
+            return
+        }
+
+        const { id } = this.props.match.params
+
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        })
+        this.player.attachMediaElement(this.videoRef.current)
+        this.player.load()
     }
 
 
@@ -13,8 +49,9 @@ class StreamShow extends React.Component {
             return <div>Loading...</div>
         }
         return ( 
-            <div class="show-stream">
+            <div className="show-stream">
                 <h2>View a stream</h2>
+                <video ref={this.videoRef} style={{ width: '100%'}} controls />
                 <h1>{this.props.stream.title}</h1>
                 <h5>{this.props.stream.description}</h5>
             </div>
